313e0795ac6483e491cb2f5564b1cbb467c8bd9e
wumacms <wumacms@qq.com>
2026-04-13 01:37:04 +0800

    fix: 移除未使用的import

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 import { useAuthStore } from '../stores/auth'
 
