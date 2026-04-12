3034169d722321669c3354e755a4005bcdea1ec8
wumacms <wumacms@qq.com>
2026-04-13 00:47:32 +0800

    feat: 支持自定义域名 base path 配置

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/dapanxia-supabase/',
+  base: import.meta.env.VITE_APP_BASE_PATH || '/dapanxia-supabase/',
   plugins: [vue(), tailwindcss()],
   server: {
     proxy: {
